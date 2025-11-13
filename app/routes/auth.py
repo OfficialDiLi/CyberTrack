b56696daa4977f9ac549fac597185de07fd81732
Yahaya Minkail @DiLi <yimmiy1389@gmail.com>
2025-11-13 13:24:32 +0100

    Upgraded

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
+from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify, session
 from flask_login import login_user, logout_user, login_required, current_user
 from app.models import User, db
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -15,6 +15,7 @@ def login():
         
         if user and check_password_hash(user.password_hash, password):
             login_user(user)
+            session.permanent = False
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('main.dashboard'))
         else:
